b58aa3f69054e3ffa8d4bbfdf04e6a12a912d60a
panrenhua <982051078@qq.com>
2020-12-03 13:55:50 +0800

    finish 204.计数质数

---
+++ leetcode_204.h
@@ -0,0 +1,25 @@
+#ifndef CPP_LEETCODE_204_H
+#define CPP_LEETCODE_204_H
+/* 204. 计数质数
+ * */
+#include "allheaders.h"
+
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<int> p(n, 0);
+        int count = 0;
+        if (n <= 2) return 0;
+
+        for (int i = 2; i <= n / 2; i++) {
+            for (int j = 2; j * i < n; j++) {
+                p[i*j] = 1;
+            }
+        }
+        for (int i = 2; i < n; i++) {
+            if (p[i] == 0) count++;
+        }
+        return count;
+    }
+};
+#endif //CPP_LEETCODE_204_H
