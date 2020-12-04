6e4cffa8ffba901ed9c3239982d1bcf773052a8f
panrenhua <982051078@qq.com>
2020-12-04 11:28:15 +0800

    321. 拼接最大数

--- leetcode_659.h
+++ leetcode_659.h
@@ -7,33 +7,7 @@
  *
  * 2. 遍历数组 考虑将当前数字插入到已有数组 == 贪心算法
  * 若要新建数组则必须要满足 nums[i] + 1, nums[i] + 2 仍存在
- *  * class Solution {
-public:
-    bool isPossible(vector<int>& nums) {
-        unordered_map<int, priority_queue<int, vector<int>,greater<int>>> mp;
-        for (auto& x : nums) {
-            if (mp.find(x) == mp.end()) {
-                mp[x] = priority_queue<int, vector<int>,greater<int>>();
-            }
-            if (mp.find(x - 1) != mp.end()) {
-                int prevLength = mp[x - 1].top();
-                mp[x - 1].pop();
-                if (mp[x - 1].empty()) {
-                    mp.erase(x - 1);
-                }
-                mp[x].push(prevLength + 1);
-            } else {
-                mp[x].push(1);
-            }
-        }
-        for (auto it = mp.begin(); it != mp.end(); ++it) {
-            priority_queue<int, vector<int>,greater<int>> queue = it->second;
-            if (queue.top() < 3) {
-                return false;
-            }
-        return true;
-    }
-};
+ *  *
  * */
 class Solution{
 public:
