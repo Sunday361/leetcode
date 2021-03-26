631941a5dacc93fad64eef86aaf3b3fb9b2acf6d
panrenhua <982051078@qq.com>
2021-03-26 12:46:05 +0800

    94. 二叉树的中序遍历

---
+++ leetcode_94.h
@@ -0,0 +1,30 @@
+//
+// Created by panrenhua on 3/26/21.
+//
+
+#ifndef LEETCODE_LEETCODE_94_H
+#define LEETCODE_LEETCODE_94_H
+#include "allheaders.h"
+/** 94. 二叉树的中序遍历
+ * */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<TreeNode*> stk;
+        vector<int> ans;
+        while (root != nullptr || !stk.empty()) {
+            while (root != nullptr) {
+                stk.emplace_back(root);
+                root = root->left;
+            }
+
+            root = stk.back();
+            stk.pop_back();
+            ans.push_back(root->val);
+            root = root->right;
+        }
+
+        return ans;
+    }
+};
+#endif //LEETCODE_LEETCODE_94_H
