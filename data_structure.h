48fab7100d93630f40ccc8165c0444b24c2490b7
panrenhua <982051078@qq.com>
2021-04-04 16:04:39 +0800

    数据结构

--- data_structure.h
+++ data_structure.h
@@ -180,4 +180,57 @@ public:
         tree_[node] = tree_[left_node] + tree_[right_node];
     }
 };
+
+namespace st{
+    vector<int> a;
+    vector<int> d;
+    vector<int> b;
+    void build(int s, int t, int p) {
+        // 对 [s,t] 区间建立线段树,当前根的编号为 p
+        if (s == t) {
+            d[p] = a[s];
+            return;
+        }
+        int m = (s + t) / 2;
+        build(s, m, p * 2), build(m + 1, t, p * 2 + 1);
+        // 递归对左右区间建树
+        d[p] = d[p * 2] + d[(p * 2) + 1];
+    }
+
+    void update(int l, int r, int c, int s, int t, int p) {
+        // [l,r] 为修改区间,c 为被修改的元素的变化量,[s,t] 为当前节点包含的区间,p
+        // 为当前节点的编号
+        if (l <= s && t <= r) {
+            d[p] += (t - s + 1) * c, b[p] += c;
+            return;
+        }  // 当前区间为修改区间的子集时直接修改当前节点的值,然后打标记,结束修改
+        int m = (s + t) / 2;
+        if (b[p] && s != t) {
+            // 如果当前节点的懒标记非空,则更新当前节点两个子节点的值和懒标记值
+            d[p * 2] += b[p] * (m - s + 1), d[p * 2 + 1] += b[p] * (t - m);
+            b[p * 2] += b[p], b[p * 2 + 1] += b[p];  // 将标记下传给子节点
+            b[p] = 0;                                // 清空当前节点的标记
+        }
+        if (l <= m) update(l, r, c, s, m, p * 2);
+        if (r > m) update(l, r, c, m + 1, t, p * 2 + 1);
+        d[p] = d[p * 2] + d[p * 2 + 1];
+    }
+
+    int getsum(int l, int r, int s, int t, int p) {
+        // [l,r] 为查询区间,[s,t] 为当前节点包含的区间,p为当前节点的编号
+        if (l <= s && t <= r) return d[p];
+        // 当前区间为询问区间的子集时直接返回当前区间的和
+        int m = (s + t) / 2;
+        if (b[p]) {
+            // 如果当前节点的懒标记非空,则更新当前节点两个子节点的值和懒标记值
+            d[p * 2] += b[p] * (m - s + 1), d[p * 2 + 1] += b[p] * (t - m),
+            b[p * 2] += b[p], b[p * 2 + 1] += b[p];  // 将标记下传给子节点
+            b[p] = 0;                                    // 清空当前节点的标记
+        }
+        int sum = 0;
+        if (l <= m) sum = getsum(l, r, s, m, p * 2);
+        if (r > m) sum += getsum(l, r, m + 1, t, p * 2 + 1);
+        return sum;
+    }
+}
 #endif //LEETCODE_DATA_STRUCTURE_H
