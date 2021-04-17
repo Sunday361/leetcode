71a4c03b764becb91e94d644d3d887a05f8bb26f
panrenhua <982051078@qq.com>
2021-04-17 10:43:04 +0800

    220. 存在重复元素 III

--- leetcode_220.h
+++ leetcode_220.h
@@ -7,19 +7,35 @@
 
 #include "allheaders.h"
 /** 220. 存在重复元素 III
+ * 每个桶放 [x - t, x + t] 的元素
  * */
 class Solution {
 public:
+    int getId(int num, long t) {
+        return num >= 0 ? num / t : (num + 1LL) / t - 1;
+    }
     bool containsNearbyAlmostDuplicate(vector<int> &nums, int k, int t) {
-        set<long> s;
-        for (int i = 0; i < nums.size(); ++i) {
-            auto pos = s.lower_bound(long(nums[i]) - t);
-            if (pos != s.end() && *pos <= long(nums[i]) + t) {
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < nums.size(); i++) {
+            long x = nums[i];
+            int id = getId(x, t + 1LL);
+
+            if (mp.count(id)) {
                 return true;
             }
-            s.insert(nums[i]);
-            if (s.size() > k) {
-                s.erase(nums[i - k]);
+
+            if (mp.count(id - 1) && abs(x - mp[id - 1]) <= t) {
+                return true;
+            }
+
+            if (mp.count(id + 1) && abs(x - mp[id + 1]) <= t) {
+                return true;
+            }
+
+            mp[id] = x;
+            if (i >= k) {
+                mp.erase(getId(nums[i - k], t + 1LL));
             }
         }
         return false;
